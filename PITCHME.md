61d9948280222d0ba4779af1ce211d66eda86cf2
Tom Merrihew <tom@gatherhere.com>
2018-04-19 13:16:23 -0400

    Presentation

--- PITCHME.md
+++ PITCHME.md
@@ -2,7 +2,6 @@
 # Electron & Node.js
 
 ---
-
 @title[Sample With Annotations]
 
 ```javascript
@@ -12,3 +11,13 @@ const app = electron.app
 
 @[1](Require Electron)
 @[2](Grab the app)
+
+---
+@title[Sample using source file]
+
++++?code=presentation/01-intro.js&lang=javascript
+
+@[1](Require `electron`)
+@[3-15](`ready` event)
+@[17-21]
+@[23-27]
---
+++ presentation/01-intro.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow, Tray } = require('electron')
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({width: 800, height: 600})
+    
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
